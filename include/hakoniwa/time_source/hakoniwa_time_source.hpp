a786b464a653deab21fed6fddb46b8db0bd19aa4
Takashi Mori <kanetugu2015@gmail.com>
2026-01-04 08:40:31 +0900

    add impl for hako time

--- include/hakoniwa/time_source/hakoniwa_time_source.hpp
+++ include/hakoniwa/time_source/hakoniwa_time_source.hpp
@@ -1,27 +1,28 @@
 #pragma once
 
 #include "hakoniwa/time_source/time_source.hpp"
+#include "hako_asset.h"
 #include <atomic>
 
 namespace hakoniwa::time_source {
 
 class HakoniwaTimeSource : public ITimeSource {
 public:
-    HakoniwaTimeSource() : current_time_micros_(0) {}
+    HakoniwaTimeSource() {}
 
     virtual uint64_t get_microseconds() const override {
-        return current_time_micros_.load();
+        hako_time_t t = hako_asset_simulation_time();
+        return static_cast<uint64_t>(t);
     }
 
     virtual void advance_time(uint64_t microseconds) override {
-        current_time_micros_ += microseconds;
+        //noop
     }
     virtual void sleep_delta_time() const override {
-        // No actual sleeping in Hakoniwa time source
+        hako_time_t sleep_time_usec = static_cast<hako_time_t>(delta_time_microseconds_);
+        hako_asset_usleep(sleep_time_usec);
     }
 
-private:
-    std::atomic<uint64_t> current_time_micros_;
 };
 
-} // namespace hakoniwa::pdu::bridge
+} // namespace hakoniwa::time_source
