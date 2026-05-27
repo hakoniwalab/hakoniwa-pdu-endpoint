b7e7e36241fbcf575f530e929b3d6e55c3260732
Takashi Mori <kanetugu2015@gmail.com>
2026-05-28 08:28:23 +0900

    Add UDP subscriber runner callback

--- benchmarks/runner/comm/sub_runner.hpp
+++ benchmarks/runner/comm/sub_runner.hpp
@@ -2,6 +2,8 @@
 
 #include "runner.hpp"
 
+#include <atomic>
+
 namespace benchmarks::runner {
 
 class SubShmRunner : public Runner {
@@ -23,6 +25,10 @@ public:
     void prepare(int num, std::string endpoint_config_path) override;
     void run() override;
     void cleanup() override;
+
+private:
+    std::atomic<int> expected_count_{0};
+    std::atomic<int> received_count_{0};
 };
 
 }
