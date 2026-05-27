cf87356cfc5e5ffb373ba65c9c5d1b9ca6642174
Takashi Mori <kanetugu2015@gmail.com>
2026-05-28 06:59:58 +0900

    refactored the runner methods

--- benchmarks/runner/comm/runner.hpp
+++ benchmarks/runner/comm/runner.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <memory>
 #include "hakoniwa/pdu/endpoint.hpp"
 
 namespace benchmarks::runner {
@@ -8,12 +9,12 @@ namespace benchmarks::runner {
 class Runner {
 public:
     virtual ~Runner() = default;
-    virtual void prepare(std::string endpoint) = 0;
+    virtual void prepare(std::string endpoint_config_path) = 0;
     virtual void run() = 0;
     virtual void cleanup() = 0;
 
 protected:
-    hakoniwa::pdu::Endpoint* endpoint_ = nullptr;
+    std::unique_ptr<hakoniwa::pdu::Endpoint> endpoint_;
 };
 
 }
