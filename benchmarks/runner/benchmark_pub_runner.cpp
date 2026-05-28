8e185a09e90c43314659bc1547a61017dae6efff
Takashi Mori <kanetugu2015@gmail.com>
2026-05-28 09:18:47 +0900

    add udp smork test

--- benchmarks/runner/benchmark_pub_runner.cpp
+++ benchmarks/runner/benchmark_pub_runner.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "comm/pub_runner.hpp"
 
-int main() {
-    std::cout << "Running benchmark_pub_runner" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <endpoint_config_path>" << std::endl;
+        return 1;
+    }
+    std::string endpoint_config_path = argv[1];
+    
+    benchmarks::runner::PubUdpRunner runner;
+    runner.prepare(1, endpoint_config_path);
+    runner.run();
+    runner.cleanup();
+
     return 0;
 }
--- benchmarks/runner/benchmark_sub_runner.cpp
+++ benchmarks/runner/benchmark_sub_runner.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "comm/sub_runner.hpp"
 
-int main() {
-    std::cout << "Running benchmark_sub_runner" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <endpoint_config_path>" << std::endl;
+        return 1;
+    }
+    std::string endpoint_config_path = argv[1];
+    
+    benchmarks::runner::SubUdpRunner runner;
+    try {
+        runner.prepare(1, endpoint_config_path);
+        runner.run();
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    runner.cleanup();
     return 0;
 }
