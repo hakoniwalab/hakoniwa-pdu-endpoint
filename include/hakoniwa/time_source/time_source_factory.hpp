5e8d412dca9dcf6c41456d273968714b077e2e99
Takashi Mori <kanetugu2015@gmail.com>
2026-07-13 06:34:04 +0900

    compilation guard is added for hakoniwa

--- include/hakoniwa/time_source/time_source_factory.hpp
+++ include/hakoniwa/time_source/time_source_factory.hpp
@@ -3,7 +3,12 @@
 #include "hakoniwa/time_source/time_source.hpp"
 #include "hakoniwa/time_source/real_time_source.hpp"
 #include "hakoniwa/time_source/virtual_time_source.hpp"
+#ifndef HAKO_PDU_ENDPOINT_DISABLE_HAKONIWA_CORE
 #include "hakoniwa/time_source/hakoniwa_time_source.hpp"
+#endif
+
+#include <stdexcept>
+#include <string>
 
 namespace hakoniwa::time_source {
 static inline std::unique_ptr<ITimeSource> create_time_source(
@@ -16,12 +21,17 @@ static inline std::unique_ptr<ITimeSource> create_time_source(
         time_source = std::make_unique<RealTimeSource>();
     } else if (type == "virtual") {
         time_source = std::make_unique<VirtualTimeSource>();
+#ifndef HAKO_PDU_ENDPOINT_DISABLE_HAKONIWA_CORE
     } else if (type == "hakoniwa") {
         time_source = std::make_unique<HakoniwaTimeSource>(HakoniwaTimeSource::ImplType::Poll);
     } else if (type == "hakoniwa_poll") {
         time_source = std::make_unique<HakoniwaTimeSource>(HakoniwaTimeSource::ImplType::Poll);
     } else if (type == "hakoniwa_callback") {
         time_source = std::make_unique<HakoniwaTimeSource>(HakoniwaTimeSource::ImplType::Callback);
+#else
+    } else if (type == "hakoniwa" || type == "hakoniwa_poll" || type == "hakoniwa_callback") {
+        throw std::invalid_argument("Hakoniwa core time source is disabled: " + type);
+#endif
     } else {
         throw std::invalid_argument("Unknown time source type: " + type);
     }
